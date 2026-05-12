9fc2d85053e1248e51500b0d99c6c15ae583f511
albyalex96 <72573722+albyalex96@users.noreply.github.com>
2026-05-12 23:19:59 +0200

    Update streamingcommunity URL for fetching streams

--- providers/streamingcommunity.js
+++ providers/streamingcommunity.js
@@ -23,7 +23,7 @@ var __async = (__this, __arguments, generator) => {
 module.exports = {
   getStreams: (id, type, season, episode) => __async(null, null, function* () {
     try {
-      const url = `https://easystreams.realbestia.com/resolve/streamingcommunity?id=${id}&type=${type}&s=${season || 1}&ep=${episode || 1}`;
+      const url = `https://maxmehlab91-es.hf.space/resolve/streamingcommunity?id=${id}&type=${type}&s=${season || 1}&ep=${episode || 1}`;
       const response = yield fetch(url);
       const data = yield response.json();
       return data.streams || [];
